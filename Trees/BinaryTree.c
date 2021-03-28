4b6d107cfcebb00880678e65826b54ce456ae4f9
Rayato159 <Rayato159@gmail.com>
2021-03-29 00:21:01 +0700

    change files name

---
+++ Trees/BinaryTree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+}front = NULL, rear = NULL;
+
+void create(){
+    struct Node *p, *t;
+    int x;
+    Queue q;
+}
+
+int main(){
+
+    return 0;
+}
