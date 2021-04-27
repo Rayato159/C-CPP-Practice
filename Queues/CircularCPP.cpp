9c23b9f8866eed7c6ce54ce34b440736c9195da0
Rayato159 <rayato159@gmail.com>
2021-04-27 21:41:02 +0700

    added Circular Queue cpp

---
+++ Queues/CircularCPP.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Queue{
+    private:
+        int size; 
+        int front, rear;
+        int *Q;
+    public:
+        Queue(int size){
+            this->size = size+1;
+            front = rear = 0;
+            Q = new int[this->size];
+        }
+
+        void enqueue(int x){
+            if((rear+1)%size == front){
+                cout << "Queue is full." << endl;
+            }
+            else{
+                rear = (rear+1)%size;
+                Q[rear] = x;
+            }
+        }
+
+        int dequeue(){
+            int x = -1;
+            if(rear == front){
+                cout << "Queue is empty" << endl;
+            }
+            else{
+                front = (front+1)%size;
+                x = Q[front];
+            }
+            return x;
+        }
+
+        void display(){
+            int i=front+1;
+            do{
+                cout << Q[i] << " " << flush;
+                i=(i+1)%size;
+            }while(i!=(rear+1)%size);
+            cout << endl;
+        }
+};
+
+int main(){
+    Queue q(3);
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+
+    q.dequeue();
+
+    q.display();
+
+    return 0;
+}
